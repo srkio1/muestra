b54817d78c66842acdf5fd208bdaac8cae83a265
Carlos Daniel RS <ingdanielruizsaucedo@gmail.com>
2023-11-29 15:39:05 -0400

    Pagina 1 con tamaños con porcentajes

--- lib/pagina1.dart
+++ lib/pagina1.dart
@@ -3,6 +3,19 @@ import 'package:flutter/material.dart';
 class Pagina1 extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Center(child: Text('Página 1')); 
+    return SingleChildScrollView(
+      child: Column(children:[
+        Container(
+        color: Colors.yellow,
+        height: MediaQuery.of(context).size.height * 0.50,
+        width: MediaQuery.of(context).size.width * 0.50,
+      ),
+      Container(
+        color: Colors.red,
+        height: MediaQuery.of(context).size.height * 0.50,
+        width: MediaQuery.of(context).size.width * 0.50,
+      )
+      ] ),
+    ); 
   }
 }
